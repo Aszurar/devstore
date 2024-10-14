230229cee0b0cbdcec042b3abd7c6ac03b5d4f70
Lucas de Lima Martins de Souza <lms.souza39@gmail.com>
2024-10-13 21:08:38 -0300

    Add CartProvider to StoreLayout component

--- src/app/(store)/layout.tsx
+++ src/app/(store)/layout.tsx
@@ -1,4 +1,5 @@
 import { Header } from '@/components/Header'
+import { CartProvider } from '@/contexts/cart'
 
 type StoreLayoutProps = {
   children: React.ReactNode
@@ -6,9 +7,11 @@ type StoreLayoutProps = {
 
 export default function StoreLayout({ children }: StoreLayoutProps) {
   return (
-    <div className="mx-auto min-h-screen p-8 w-full max-w-400 gap-5 grid grid-rows-app">
-      <Header />
-      {children}
-    </div>
+    <CartProvider>
+      <div className="mx-auto min-h-screen p-8 w-full max-w-400 gap-5 grid grid-rows-app">
+        <Header />
+        {children}
+      </div>
+    </CartProvider>
   )
 }
